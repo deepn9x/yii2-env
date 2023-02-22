884b34a892b7f7fd21fb4e7e0a42f8f2e1550ff4
deepn9x <105905442+deepn9x@users.noreply.github.com>
2023-02-22 23:22:51 +0500

    Update dote.php

--- dote.php
+++ dote.php
@@ -4,27 +4,24 @@ namespace deepn9x\env;
 
 /**
  * This is just an example.
+ * Author: DEEPN9X
  */
 class dote
 {
-
     private $word;
     public $key;
 
     public function __construct($key)
     {
-
         $envDir = \Yii::getAlias('@app') . '/config/.env';
         $envDir = file_get_contents($envDir);
         $env = explode(PHP_EOL, $envDir);
         $list = [];
-
         foreach ($env as $item) {
             if ($item != '') {
                 $env_explode = explode('=', $item);
                 $list[$env_explode[0]] = $env_explode[1];
             }
-
         }
         if ($this->key) {
             if (isset($list[$this->key])) {
@@ -32,7 +29,6 @@ class dote
                 $this->word = $word;
                 return $this->word;
             } else {
-
                 $this->word = 'null';
                 return $this->word;
             }
@@ -42,25 +38,17 @@ class dote
                 $this->word = $word;
                 return $this->word;
             } else {
-
                 $this->word = 'null';
                 return $this->word;
             }
         }
-
-        //Build by DEEPN9X
     }
-
     public function key($key)
     {
         return $this->__construct($key);
-//        $this->key = $key;
     }
-
     public function __toString()
     {
         return $this->word;
     }
-
-
 }
