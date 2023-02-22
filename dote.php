034c6a4a3b2fa1690440799e3b5140e27ca7738c
deepn9x <105905442+deepn9x@users.noreply.github.com>
2023-02-22 22:48:24 +0500

    Add files via upload

--- dote.php
+++ dote.php
@@ -7,8 +7,39 @@ namespace deepn9x\env;
  */
 class dote
 {
-    public function __construct()
+
+    private $word;
+
+    public function __construct($key)
     {
-        return "Hello";
+        $envDir = \Yii::getAlias('@app') . '/config/.env';
+        $envDir = file_get_contents($envDir);
+        $env = explode(PHP_EOL, $envDir);
+        $list = [];
+
+        foreach ($env as $item){
+            $env_explode = explode('=', $item);
+            $list[$env_explode[0]] = $env_explode[1];
+        }
+
+        if(isset($list[$key])){
+            $word = $list[$key];
+            $this->word = $word;
+            return $this->word;
+        }
+        else{
+
+            $this->word = 'null';
+            return $this->word;
+        }
+
+
     }
+
+    public function __toString()
+    {
+        return $this->word;
+    }
+
+
 }
